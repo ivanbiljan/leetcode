5073a5dc30a8e0aa1c36c44467bbda7c9ba577e5
ivanbiljan <ssbiljan@gmail.com>
2021-04-15 21:47:33 +0200

    Optimize the algorithm used for day 15 of april leetcoding challenge

--- april-leetcoding-challenge/day15.cs
+++ april-leetcoding-challenge/day15.cs
@@ -1,18 +1,14 @@
 public class Solution {
     public int Fib(int n) {
-        // Approach #1:
-        /*if (n == 0 || n == 1) {
-            return n;
-        }
-        
-        return Fib(n - 1) + Fib(n - 2);*/
-        
-        // Approach #2:
         if (n < 2) {
             return n;
         }
         
-        var memo = new int[n + 1];
+        // Approach #1:
+        //return Fib(n - 1) + Fib(n - 2);*/
+        
+        // Approach #2:
+        /*var memo = new int[n + 1];
         memo[0] = 0;
         memo[1] = 1;
         
@@ -20,8 +16,17 @@ public class Solution {
             memo[i] = memo[i -1] + memo[i - 2];
         }
         
-        return memo[n];
+        return memo[n];*/
         
-        // Approach #3: TODO
+        // Approach #3:
+        var x = 0;
+        var y = 1;
+        for (var i = 2; i <= n; ++i) {
+            var z = x + y;
+            x = y;
+            y = z;
+        }
+        
+        return y;
     }
 }
