badd50c35023fe5a9d73bfcde9ed921e02b3f4dd
ivanbiljan <ssbiljan@gmail.com>
2021-07-19 00:33:54 +0200

    Add day 1 of July leetcoding challenge

---
+++ july-leetcoding-challenge/day01.java
@@ -0,0 +1,17 @@
+/**
+An n-bit gray code sequence is a sequence of 2n integers where:
+
+Every integer is in the inclusive range [0, 2n - 1],
+The first integer is 0,
+An integer appears no more than once in the sequence,
+The binary representation of every pair of adjacent integers differs by exactly one bit, and
+The binary representation of the first and last integers differs by exactly one bit.
+Given an integer n, return any valid n-bit gray code sequence.
+ */
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new LinkedList<>();
+        for (int i = 0; i < 1<<n; i++) result.add(i ^ i>>1);
+        return result;
+    }
+}
