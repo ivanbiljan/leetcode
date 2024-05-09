7453662e634d2cc492f44d23840a19a1359fbdb6
ivanbiljan <ssbiljan@gmail.com>
2024-05-09 09:12:28 +0200

    3075. Heap based solution

--- fresh-start-session/medium/3075-maximize-happiness-of-selected-children.cs
+++ fresh-start-session/medium/3075-maximize-happiness-of-selected-children.cs
@@ -1,12 +1,28 @@
 public class Solution {
     public long MaximumHappinessSum(int[] happiness, int k) {
-        Array.Sort(happiness);
+        // Array.Sort(happiness);
+
+        // var ans = 0L;
+        // for (var i = 0; i < k; ++i) {
+        //     ans += Math.Max(happiness[happiness.Length - i - 1] - i, 0);
+        // }
+
+        // return ans;
+
+        var heap = new PriorityQueue<int, int>(new MaxHeapComparer());
+        foreach (var h in happiness) {
+            heap.Enqueue(h, h);
+        }
 
         var ans = 0L;
         for (var i = 0; i < k; ++i) {
-            ans += Math.Max(happiness[happiness.Length - i - 1] - i, 0);
+            ans += Math.Max(heap.Dequeue() - i, 0);
         }
 
         return ans;
     }
+}
+
+public sealed class MaxHeapComparer : IComparer<int> {
+    public int Compare(int x, int y) => y.CompareTo(x);
 }
